2eff5f76cf1d4630b295fe524036f94145c2a637
wcroughan <croughanb@gmail.com>
2021-11-17 00:04:35 -0800

    update

--- loadAndPlot.py
+++ loadAndPlot.py
@@ -1,18 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 
 def main():
     fname = "/home/wcroughan/Unity/Projects/Sliders/Assets/eqvals.txt"
+    if not os.path.exists(fname):
+        fname = "/home/wcroughan/repos/music/Faders/Assets/eqvals.txt"
+
     vals = np.loadtxt(fname, delimiter=",").T
     s = np.sum(vals, axis=1)
     # vals = np.divide(vals, np.reshape(s, (-1, 1)))
     # plt.imshow(vals)
     # plt.plot(np.log(s))
-    d = np.ones_like(s)
-    for i in range(len(d)):
-        d[i] *= pow(10, float(i) / (float(len(d)) * float(1)))
-    plt.plot(np.divide(s, d))
+    e = np.linspace(0, 5, np.size(s))
+    d = np.power(2.5, e)
+    ss = np.multiply(s, d)
+    plt.plot(np.log(ss))
     plt.show()
 
 
